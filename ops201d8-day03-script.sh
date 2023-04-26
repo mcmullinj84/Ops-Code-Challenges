2d7d488b9e047facf612151477c4131f8da33d26
Jonathan <mcmullinj84@gmail.com>
2023-04-26 19:16:35 +0000

    This is the commit for Ops Challenge: Class 03

---
+++ ops201d8-day03-script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script Name:                  Class 03
+# Author:                       Jonathan McMullin
+# Date of latest revision:      04/26/2023
+# Purpose:                      Use a function to create a login history
+
+# Declaration of variables
+
+# Declaration of functions
+print_login_history () {
+    last
+    echo "This is the login history"
+}
+
+# Main
+print_login_history
+print_login_history
+print_login_history
+
+# End
