c936d6cc558a771c03ed93739009a1c600f70e45
Jonathan <mcmullinj84@gmail.com>
2023-04-26 00:41:51 +0000

    This is my first bash script - Ops Challenge Class 2

---
+++ helloworld.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script Name:                  Print Hello World
+# Author:                       Jonathan McMullin
+# Date of latest revision:      04/25/2023
+# Purpose:                      Practice printing
+
+# Declaration of variables
+greeting=Hello
+greeting_part2=World
+Perfection1="I am working perfectly!"
+# Declaration of functions
+
+
+# Main
+echo $greeting $greeting_part2
+echo
+echo $Perfection1
+
+# End
