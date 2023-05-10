de31906d12af8a3658ea7da97ac45374540a54ff
Jonathan <mcmullinj84@gmail.com>
2023-05-10 01:34:44 +0000

    This fixes the file path for day 12

--- ops201d8-day12.ps1
+++ ops201d8-day12.ps1
@@ -11,10 +11,10 @@ function Search-IPv4Address {
     ipconfig > C:\Users\VM-JMC\Desktop\network_report.txt
 
     # Search .txt file for IPv4 address and output that line into Powershell
-    $ipv4Addresses = Select-String -Path network_report.txt -Pattern "IPv4 Address" | Select-Object -ExpandProperty Line
+    $ipv4Addresses = Select-String -Path C:\Users\VM-JMC\Desktop\network_report.txt -Pattern "IPv4 Address" | Select-Object -ExpandProperty Line
 
     # Remove network_report.txt file
-    Remove-Item network_report.txt
+    Remove-Item C:\Users\VM-JMC\Desktop\network_report.txt
 
     # Return the IPv4 addresses as output from the function
     return $ipv4Addresses
