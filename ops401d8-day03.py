123821793303ca5c8ffa220f735e1d4d9d99baad
Jonathan <mcmullinj84@gmail.com>
2023-07-13 16:54:33 +0000

    improved version

--- ops401d8-day03.py
+++ ops401d8-day03.py
@@ -5,34 +5,108 @@
 # Date of latest revision:      07/12/2023
 # Purpose:                      Uptime Sensor Tool w/ Email Alert
 
+#credit to my instructor Marco for structure for check_ping function
+
 # import Libraries
 import os
 import datetime
 import time
 import smtplib
-from email.message import EmailMessage
 from getpass import getpass
 
+email = input("Enter your email address: ")
+# use "app password" not, normal password
+password = getpass("Enter your password: ")
+ip = input("Enter an IP address: ")
+up = "Host in active"
+down = "Host is down"
+
+# check status change
+last = 0
+ping_result = 0 
+
+# Functions 
+
+#Function to handle down alert (change from up to down)
+def send_upAlert():
+    now = datetime.datetime.now()
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+
+    #start TLS
+    s.starttls()
+
+    #Authentication
+    s.login(email, password)
+
+    message = "Your server is back up"
+
+    # send the email
+    s.sendmail("mailbot@codefellows.com", email, message)
+
+    # Close the session
+    s.quit()
+
+#Function to handle down alert (change from down to up)
+def send_downAlert():
+    now = datetime.datetime.now()
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+
+    #start TLS
+    s.starttls()
+
+    #Authentication
+    s.login(email, password)
+
+    message = "Your server is down"
+
+    # send the email
+    s.sendmail("mailbot@codefellows.com", email, message)
+
+    # Close the session
+    s.quit()
+
+
+def check_ping():
+    now = datetime.datetime.now()
+    global ping_result # starts as 0
+    global last # starts as 0
+
+    # Sends a single ping to target and puts the response into a variable
+    #response = os.system("ping -c 1 " + target)
+
+    # Check the change of ping status from up to down and down to up
+
+    if ((ping_result != last) and (ping_result == up)):
+        
+        # Change value of last
+        last = up
+        
+        # Calls function to send email
+        send_upAlert()
+
+    elif ((ping_result != last) and (ping_result == down)):
+        
+        # Change value of last
+        last = down
+        # Calls function to send email
+        send_downAlert()
+    
+    # Ping User inputted IP address and put it into local variable "response" 
+    response = os.system("ping -c 1 " + ip)
+
+    # Evaulate the ping response
+    if response == 0:
+        ping_result = up 
+    else:
+        ping_result = down
+
+    print(str(now) + ping_result + "to " + ip)
 
 # Main 
-# Transmit a single ICMP (ping) packet to a specific IP.
-# Evaluate the response as success/failure. 0 = success
-# Credit to ChatGPT for helping with line 18 - to send output to null device
-def ping(host):
-    response = os.system("ping -c 1 " + host + " > /dev/null 2>&1")
-    return response == 0
 
-# prints the status and timestamp for destination IP tested
-def print_status(timestamp, host, status):
-    print(f"[{timestamp}] Host: {host} - Status: {'Success' if status else 'Failure'}")
-# Creates infinite loop with 2 second delay
-def uptime_sensor(host):
-    while True:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        status = ping(host)
-        print_status(timestamp, host, status)
-        time.sleep(2)
+# infinite loop
 
-#input host IP and run loop
-host_to_check = "8.8.8.8"
-uptime_sensor(host_to_check)
+while True: 
+    check_ping()
+    time.sleep(2)
+
