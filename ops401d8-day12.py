3557451586eb732bb51fe6d890a5c2ee86ae4433
Jonathan <mcmullinj84@gmail.com>
2023-07-26 16:04:25 +0000

    v2

--- ops401d8-day12.py
+++ ops401d8-day12.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3 
 
-# Script Name:                  ops401d8-day11
+# Script Name:                  ops401d8-day12
 # Author:                       Jonathan McMullin
 # Date of latest revision:      07/24/2023
 # Purpose:                      TCP Port Range Scanner
@@ -13,48 +13,54 @@
 
 
 import ipaddress
-from scapy.all import ICMP, IP, sr1
+from scapy.all import ICMP, IP, sr1, TCP, send
+
+def scan_port(host, port):
+    src_port = 22  # Replace with your desired source port
+    response = sr1(IP(dst=host) / TCP(sport=src_port, dport=port, flags="S"), timeout=1, verbose=0)
+
+    if response is None:
+        print(f"Port {port} is filtered (silently dropped).")
+    elif response.haslayer(TCP):
+        if response.getlayer(TCP).flags == 0x12:
+            send(IP(dst=host) / TCP(sport=src_port, dport=port, flags="R"), verbose=0)
+            print(f"Port {port} is open.")
+        elif response.getlayer(TCP).flags == 0x14:
+            print(f"Port {port} is closed.")
+    else:
+        print(f"Port {port} is in an unknown state.")
 
 def ping_sweep():
     global ip_list
     global host_count
 
-    # Send ICMP request to each host on list
-    for host in ip_list:
-        # Skip network address and broadcast address
-        if host in (ip_list.network_address, ip_list.broadcast_address):
-            continue
-            
-        response = sr1(IP(dst=str(host)) / ICMP(), timeout=1, verbose=0)
-
-        if response is None:
-            print(f"Host {host} is down or unresponsive.")
-        elif int(response.getlayer(ICMP).type) == 3 and int(response.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]:
-            print(f"Host {host} is actively blocking ICMP traffic.")
-        else:
-            print(f"Host {host} is responding.")
-            host_count += 1 
-
 def main():
     global ip_list
     global host_count
 
-    # Prompt user for network address in CIDR notation
-    network = input("Enter network address in CIDR notation (e.g., x.x.x.x/24): ")
-    ip_list = ipaddress.IPv4Network(network).hosts()
-    host_count = 0
-
-    # Display menu and perform actions based on user choice
     while True:
-        print("ICMP Ping Sweep Tool Menu:")
-        print("1. Perform ICMP Ping Sweep")
-        print("2. Quit")
+        print("Tool Menu:")
+        print("1. ICMP Ping Sweep")
+        print("2. TCP Port Range Scanner")
+        print("3. Quit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
+            # ICMP Ping Sweep mode
+            network = input("Enter network address in CIDR notation (e.g., x.x.x.x/24): ")
+            ip_list = ipaddress.IPv4Network(network).hosts()
+            host_count = 0
             ping_sweep()
             print(f"Total hosts online: {host_count}")
         elif choice == "2":
+            # TCP Port Range Scanner mode
+            host = input("Enter the target hostname or IP address: ")
+            port_range_str = input("Enter the port range (e.g., 20-80): ")
+            start_port, end_port = map(int, port_range_str.split("-"))
+            port_range = range(start_port, end_port + 1)
+            for port in port_range:
+                scan_port(host, port)
+        elif choice == "3":
             print("Goodbye!")
             break
         else:
