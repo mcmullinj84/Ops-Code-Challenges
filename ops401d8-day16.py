024a17114b9467d5c72cf97203bcd2ab784f62c4
Jonathan <mcmullinj84@gmail.com>
2023-08-01 16:23:42 +0000

    using getpass

--- ops401d8-day16.py
+++ ops401d8-day16.py
@@ -9,14 +9,14 @@
 
 # Import Libraries
 import time
-
+from getpass import getpass 
 # Declare Functions
 
 def iterator():
     # Ask the user for the filepath to the wordlist
     filepath = input("Input the complete filepath for the wordlist: ")
 
-    # Open the wordlist file using the 'with' statement to ensure proper file handling
+    # Open the wordlist file using the 'with' statement to ensure proper file handling (Chat GPT Recommended)
     with open(filepath, 'r') as file:
         line = file.readline()
         print(line)
@@ -31,11 +31,13 @@ def iterator():
 
             # Get the next line in the wordlist
             line = file.readline()
+        file.close()
 
 def password_recognized():
-    user_input = input("Enter a string: ")
+    user_input = getpass("Enter a string: ")
     filepath = input("Input the complete filepath for the wordlist: ")
 
+    # 'Try' runs script as long as no errors occur, 'except' handles the error
     try:
         with open(filepath, 'r') as file:
             wordlist = [line.strip() for line in file]
