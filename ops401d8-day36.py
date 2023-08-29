bf15d36197c7e233957944c22d973bb0b3c1ad22
Jonathan <mcmullinj84@gmail.com>
2023-08-29 17:00:51 +0000

    added subprocess for nmap

--- ops401d8-day36.py
+++ ops401d8-day36.py
@@ -4,34 +4,40 @@
 # Author:                       Jonathan McMullin
 # Date of latest revision:      08/28/2023
 # Purpose:                      Web Application Fingerprinting
+
 # Credit: Utilized syntax from Code Fellows Demo and ChatGPT to develop this script
 
 import subprocess
+import socket
 
 def banner_grab_netcat(target, port):
     try:
-        # Use subprocess to run netcat and capture its output
-        command = f'nc -v {target} {port}'
-        result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+        # Create a socket and connect to the target
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5)  # Set a timeout for the connection attempt
+            s.connect((target, port))
+            banner = s.recv(1024).decode()
         print("Netcat Banner Grabbing:")
-        print(result)
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e.output}")
+        print(banner)
+    except (socket.error, TimeoutError) as e:
+        print(f"Error: {e}")
 
 def banner_grab_telnet(target, port):
     try:
-        # Use subprocess to run telnet and capture its output
-        command = f'telnet {target} {port}'
-        result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+        # Create a socket and connect to the target
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(5)  # Set a timeout for the connection attempt
+            s.connect((target, port))
+            banner = s.recv(1024).decode()
         print("\nTelnet Banner Grabbing:")
-        print(result)
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e.output}")
+        print(banner)
+    except (socket.error, TimeoutError) as e:
+        print(f"Error: {e}")
 
 def banner_grab_nmap(target):
     try:
-        # Use subprocess to run Nmap and capture its output
-        command = f'nmap -p 1-65535 -sV -Pn {target}'
+        # Run Nmap to scan the most popular ports (1-1000)
+        command = f'nmap -p 1-1000 -sV -Pn {target}'
         result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
         print("\nNmap Banner Grabbing:")
         print(result)
@@ -41,7 +47,7 @@ def banner_grab_nmap(target):
 def main():
     # Prompt the user for target address and port number
     target = input("Enter the URL or IP address: ")
-    port = input("Enter the port number: ")
+    port = int(input("Enter the port number: "))  # Convert port to an integer
 
     # Perform banner grabbing using netcat
     banner_grab_netcat(target, port)
